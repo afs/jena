7e787581cf46710da194cf0c963599981fc265ab
Andy Seaborne <andy@apache.org>
2024-10-16 19:44:56 +0100

    WIP: IRI3986

---
+++ jena-core/src/main/java/org/apache/jena/iri3986/provider/InitIRI3986.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jena.iri3986.provider;
+
+import org.apache.jena.irix.IRIProvider;
+import org.apache.jena.irix.SystemIRIx;
+import org.seaborne.rfc3986.ErrorHandler;
+import org.seaborne.rfc3986.SystemIRI3986;
+import org.seaborne.rfc3986.Violations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Initialize IRI3986 as the system-wide IRIx provider.
+ */
+public class InitIRI3986 {
+
+    private static Logger LOG = LoggerFactory.getLogger("IRI3986");
+
+    private static IRIProvider systemtDefaultProvider = null;
+
+    // Call, or move code to, SystemIRIx.init
+    public static void init() {
+        if ( systemtDefaultProvider == null )
+            systemtDefaultProvider = SystemIRIx.getProvider();
+
+        IRIProvider3986 provider = new IRIProvider3986();
+        // Errors and warnings should be handled via exceptions and violations.
+        // In case any aren't, set the IRI3986 system error handler
+        ErrorHandler iri3986errorHandler = ErrorHandler.create(msg->LOG.error(msg), msg->LOG.warn(msg));
+        SystemIRI3986.setErrorHandler(iri3986errorHandler);
+        Violations.setSystemSeverityMap(JenaSeveritySettings.jenaSystemSettings());
+        SystemIRIx.setProvider(provider);
+    }
+}
--- jena-core/src/test/java/org/apache/jena/irix/TS_IRIx.java
+++ jena-core/src/test/java/org/apache/jena/irix/TS_IRIx.java
@@ -18,23 +18,37 @@
 
 package org.apache.jena.irix;
 
-import junit.framework.JUnit4TestAdapter;
-import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+//import org.junit.platform.suite.api.SelectClasses;
+//import org.junit.platform.suite.api.Suite;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestSuite;
+
+// JUnit5. Does not mix with JUnit3. So until jena-core updates to JUnit 4 or 5 ...
+//@Suite
+//@SelectClasses({
 @RunWith(Suite.class)
 @Suite.SuiteClasses( {
-    // IRIx tests with matrix of providers.
-    TestIRIxSyntax.class,
-    TestIRIxOps.class,
-    TestRFC3986.class,
-    TestResolve.class,
-    TestNormalize.class,
-    TestRelative.class
-} )
+    // RFC3986 syntax only
+    TestIRIxSyntaxRFC3986.class,
+    // Contract for Jena, including schema violations.
+    TestIRIxJenaSystem.class,
+    // Operations on IRIx
+    TestIRIxAbsoluteRelative.class,
+    TestIRIxNormalize.class,
+    TestIRIxReference.class,
+    TestIRIxRelative.class,
+    TestIRIxRelativize.class,
+    TestIRIxResolve.class,
 
+    // odds and ends that come up
+    TestIRIxOther.class
+} )
 public class TS_IRIx {
+    // Integration into jena-core testing.
     public static TestSuite suite() {
         TestSuite ts = new TestSuite();
         ts.setName("IRIx");
